13bcd2276adda2411516fe4ad258a3531e2861af
Thomas Daugaard <daugaard@gmail.com>
2016-12-12 09:58:09 +0100

    Remove unused code and improve display

--- modules/Token.js
+++ modules/Token.js
@@ -4,11 +4,11 @@ const CommandModule = require('../CommandModule')
 const logger = require('../logger')
 const Common = require('../common')
 const numeral = require('numeral')
-const median = require('median')
 const moment = require('moment')
 const request = require('request')
 const deferred = require('deferred')
 const cachedRequest = require('cached-request')(request)
+const MessageEmbed = require('./util/MessageEmbed')
 
 class TokenModule extends CommandModule {
     constructor (parent, config) {
@@ -43,55 +43,34 @@ class TokenModule extends CommandModule {
         return defer.promise
     }
 
-    /*_getBestTimeEver(data) {
-        let timeMap = {}
-
-        data.forEach(v => {
-            const [time, price] = v
-            const hour = moment.unix(time).format('HH')
-
-            if (!timeMap.hasOwnProperty(hour)) {
-                timeMap[hour] = []
-            }
-
-            timeMap[hour].push(price)
-        })
-
-        for (const k of Object.keys(timeMap)) {
-            timeMap[k] = median(timeMap[k])
-        }
-
-        return result = {
-            buy: '',
-            sell: ''
-        }
-    }*/
-
     Message (message) {
-        const defer = deferred()
-
-        this.getHistory()
+        return this.getHistory()
             .then(json => {
                 const region = this.bot.config.warcraftlogs.region.toUpperCase()
                 const data = json.update[region]
-                //const bestTimeEver = this._getBestTimeEver(json.history[region])
-                const authorId = message.author.id
                 const goldPerDayToPlayForFree = numeral(data.raw.buy / 30).format('0,0')
-                const str = `<@${authorId}>, https://wowtoken.info/ reports that` +
-                            ` on **${data.formatted.updated}**,` +
-                            ` a token sold for **${data.formatted.buy}**` +
-                            ` within **${data.formatted.timeToSell}**.` +
-                            ` In the past 24 hours, the range was **${data.formatted['24min']} - ${data.formatted['24max']}**.` +
-                            `\n\nAt the current rate, if you want to play for free, you need to make **~${goldPerDayToPlayForFree}g** per day.` +
-                            `\n\nHere's the 24 hour history graph for WoW Tokens in ${region} region:`
+                const embed = new MessageEmbed('WoW Token Price Data')
 
-                message.channel.sendFile(data.formatted.sparkurl, "WoWToken24hHistory.png", str)
-                    .then(() => defer.resolve(false))
-                    .catch(defer.reject)
+                embed.color = 3447003
+                embed.timestamp = moment.unix(data.raw.updated).toDate()
+
+                embed.addField('Current Price', data.formatted.buy, true)
+                embed.addField('24h Range', data.formatted['24min'] + ' - ' + data.formatted['24max'], true)
+                embed.addField('Sells Within', data.formatted.timeToSell, true)
+                embed.addField('Play for Free', `Make about **${goldPerDayToPlayForFree}g** per day.`, true)
+
+                embed.image = {
+                    url: data.formatted.sparkurl
+                }
+                embed.provider = {
+                    name: 'WoW Token Info',
+                    url: 'https://wowtoken.info/'
+                }
+
+                return {
+                    embed: {embed: embed}
+                }
             })
-            .catch(defer.reject)
-
-        return defer.promise
     }
 }
 
