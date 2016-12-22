8db6c52a4ecde30ed715d14326fe5a584ac2d879
Thomas Daugaard <tda@globase.com>
2016-12-22 11:23:45 +0100

    Update Self with more information
    Changed from using a mocked up MessageEmbed object to an actual RichEmbed from discord.js

--- modules/Self.js
+++ modules/Self.js
@@ -1,9 +1,9 @@
 'use strict'
 
 const CommandModule = require('../CommandModule')
+const Common = require('../common')
 const humanize = require('humanize')
-const moment = require('moment')
-const MessageEmbed = require('./lib/MessageEmbed')
+const RichEmbed = require('discord.js').RichEmbed
 const os = require('os')
 
 class SelfModule extends CommandModule {
@@ -13,24 +13,64 @@ class SelfModule extends CommandModule {
         this.addTrigger('!self', {
             'short': 'Show details about the bot.'
         })
+        this.addTrigger('!bugs', {
+            'short': '99 bugs in the code, 99 bugs in the code, take one down, patch it around, ..'
+        })
+        this.addTrigger('!good', {
+            'short': '👌👀👌👀👌👀👌👀👌👀 good shit go౦ԁ sHit👌 thats ✔ some good👌👌'
+        })
+    }
+
+    getModules () {
+        const botModules = this.bot.getModules()
+        const modules = []
+
+        for (const k of Object.keys(botModules)) {
+            const module = botModules[k]
+            const enabled = module.enabled
+            const formatting = enabled ? '**' : '~~'
+            let str = formatting + k + formatting
+
+            if (enabled) {
+                str += ' (_' + Object.keys(module.getTriggers()).join('_, _') + '_)'
+            }
+
+            modules.push(str)
+        }
+
+        return modules
+    }
+
+    _getRuntimeEnvironment () {
+        return Common.runningUnderPM() ? 'Process Manager' : 'Console'
     }
 
     Message (message) {
-        const embed = new MessageEmbed()
-        const memoryUsage = process.memoryUsage()
+        const trigger = this._getTrigger(message)
 
-        embed.color = 3447003
+        if (trigger === 'bugs') {
+            return Promise.resolve({content: 'True. fucking. story. brah.', file: 'https://i.yais.dk/zxYzAY.png'})
+        }
 
-        embed.addField('Node.JS', process.version, true)
-        embed.addField('OS', os.type() + ' (' + os.arch() + ')', true)
+        if (trigger === 'good') {
+            return Promise.resolve({content: '👌👀👌👀👌👀👌👀👌👀 good shit go౦ԁ sHit👌 thats ✔ some good👌👌shit right👌👌th 👌 ere👌👌👌 right✔there ✔✔if i do ƽaү so my selｆ 💯 i say so 💯 thats what im talking about right there right there (chorus: ʳᶦᵍʰᵗ ᵗʰᵉʳᵉ) mMMMMᎷМ💯 👌👌 👌НO0ОଠＯOOＯOОଠଠOoooᵒᵒᵒᵒᵒᵒᵒᵒᵒ👌 👌👌 👌 💯 👌 👀 👀 👀 👌👌Good shit'})
+        }
 
-        embed.addField('Started', humanize.relativeTime(moment().unix() - process.uptime()), true)
-        embed.addField('Memory Usage', humanize.filesize(memoryUsage.heapUsed), true)
+        if (trigger === 'self') {
+            const embed = new RichEmbed({color: 3447003})
+            const memoryUsage = process.memoryUsage()
 
-        // embed.addField('Channels', this.bot.discord.channels.array().length, true)
-        // embed.addField('Messages Processed', humanize.filesize(memoryUsage.heapUsed), true)
+            embed
+                .addField('Node.JS', process.version, true)
+                .addField('OS', os.type() + ' (' + os.arch() + ')', true)
+                .addField('Environment', this._getRuntimeEnvironment(), true)
+                .addField('Uptime', Common.relativeTime(process.uptime() * 1000), true)
+                .addField('Memory Usage', humanize.filesize(memoryUsage.heapUsed), true)
+                .addField('CPU Time', Common.relativeTime(process.cpuUsage().user / 1000), true)
+                .addField('Modules/Triggers', this.getModules().join(', '))
 
-        return Promise.resolve({embed: {embed: embed}})
+            return Promise.resolve({embed: {embed: embed}})
+        }
     }
 }
 
